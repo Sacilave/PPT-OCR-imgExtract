b993fb015e305d47be170c62ea6ed8d3ee9566c5
Sacilave <sacilave@gmail.com>
2025-01-12 00:28:25 +0800

    Update Run.bat & Readme

--- RUN.bat
+++ RUN.bat
@@ -23,13 +23,13 @@ if errorlevel 1 (
 )
 
 echo.
-echo [1/3] 安装依赖...
+echo [1/3] 安装依赖中...可能要很久哦，去休息一下吧~
 pip install pywin32
 python -m pip install paddlepaddle -i https://mirror.baidu.com/pypi/simple
 python -m pip install "paddleocr>=2.0.1" -i https://mirror.baidu.com/pypi/simple
 
 echo.
-echo [2/3] 转换PPT为图片...
+echo [2/3] 转换PPT为图片中...
 python "ppt_to_png.py"
 if %ERRORLEVEL% NEQ 0 (
     echo [错误] PPT转换失败！
@@ -39,7 +39,7 @@ if %ERRORLEVEL% NEQ 0 (
 )
 
 echo.
-echo [3/3] 提取关键词页面...
+echo [3/3] 提取关键词页面中...
 python "ocr_process.py"
 if %ERRORLEVEL% NEQ 0 (
     echo [错误] OCR处理失败！
